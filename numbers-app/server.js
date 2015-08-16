a7e5b4d39472384297e3234135da11e2e955b9cb
Diego Lapiduz <diego.lapiduz@gsa.gov>
2015-08-16 16:06:27 -0500

    Finish basic functionality for app

--- numbers-app/server.js
+++ numbers-app/server.js
@@ -1,8 +1,45 @@
+var _ = require('lodash');
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 
-app.get("/", function(req,res) {
-  res.send("Hello!");
+// Load env variables
+var accountSid = process.env.TWILIO_SID;
+var authToken = process.env.TWILIO_TOKEN;
+var destination = process.env.TO_NUMBER;
+var origin = process.env.FROM_NUMBER;
+
+// Create Twilio client
+var client = require('twilio')(accountSid, authToken);
+
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+app.get('/', function(req,res) {
+  res.render('index');
+});
+
+app.post('/', function(req,res) {
+  client.sms.messages.create({
+    to: destination,
+    from: origin,
+    body: req.body.name + ' is at the door.'
+  }, function(error, message) {
+    if (!error) {
+      console.log('Success! The SID for this SMS message is:');
+      console.log(message.sid);
+      console.log('Message sent on:');
+      console.log(message.dateCreated);
+    } else {
+      console.log('Oops! There was an error.');
+      console.log(error);
+    }
+  });
+
+
+  res.render('done');
 });
 
 var port = process.env.PORT;
